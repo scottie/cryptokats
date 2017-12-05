19ca14d96e620cce50bafe95e5ab49fccee50ac6
scott lindh <scottlindh@gmail.com>
2017-12-05 07:49:37 +0000

    kats command, scanner added

--- index.js
+++ index.js
@@ -18,11 +18,28 @@ program
  			program.timer = 10000
  		};
 
+		console.log("              .__....._             _.....__,");
+		console.log("                 .x: o :':         ;': o :x.x");
+		console.log("                 `. `-' .'.       .'. `-' .'   ");
+		console.log("                   `---'             `---'  ");
+		console.log("");
+		console.log("         _...----...      ...   ...      ...----..._");
+		console.log("      .-'__..-xx'----    `.  `x`  .'    ----'xx-..__`-.");
+		console.log("     '.-'   _.--xxx'       `-._.-'       'xxx--._   `-.`");
+		console.log("     '  .-x'                  :                  `.-.  `");
+		console.log("       '   ..              _.'x'._              .'   .");
+		console.log("             ..       ,.-'x       x'-.,       .'.");
+		console.log("               ..                           ..");
+		console.log("                 .-._                   _.-.");
+		console.log("                     `x.--...___...--.x.");
+		console.log("                   tradingview.com/u/console");
+
+
  		console.log("Scanner Activated");
- 		console.log(chalk.bgYellow("=*=*=*=*=*=*=*=*=*=*=*=*"));
+ 		console.log(chalk.black(chalk.bgYellow("=*=*=*=*=*=*=*=*=*=*=*=*")));
  		console.log("Kitties Under: " + program.price);
  		console.log("Fancy: " + program.fancy);
- 		console.log(chalk.bgYellow("=*=*=*=*=*=*=*=*=*=*=*=*"));
+ 		console.log(chalk.black(chalk.bgYellow("=*=*=*=*=*=*=*=*=*=*=*=*")));
  		findCats(program.price,program.fancy,program.timer);
  	}
  	else{
@@ -40,7 +57,8 @@ function underPrice(under, fancy){
 		    var num = 0;
 		    while(num <= 999 - 1){
 		    	var price = val[num].current_price / 1000000000000000000;
-	            if(price < under){       
+	            if(parseFloat(price).toFixed(4) <= parseFloat(under).toFixed(4)){    
+	                console.log(chalk.gray("Purrrrrrr...."));  
 	            	if(val[num].kitty.is_fancy.toString() == fancy.toString()) {
 		             	console.log(chalk.red("Price: ") + price);
 			            console.log("https://www.cryptokitties.co/kitty/" + val[num].kitty.id);
@@ -48,7 +66,13 @@ function underPrice(under, fancy){
 			            console.log(chalk.green("Fancy?: ") + val[num].kitty.is_fancy);
 			            console.log(chalk.green("IMG: ") + val[num].kitty.image_url);
 			            console.log(chalk.green("Name: ") + val[num].kitty.name);
+			            console.log(chalk.green("Generation: ") + val[num].kitty.generation);
+			            console.log(chalk.green("Color: ") + val[num].kitty.color);
+			            console.log(chalk.green("Is Ready: ") + val[num].kitty.is_ready);
+			            console.log(chalk.green("Cooldown Index: ") + val[num].kitty.status.cooldown_index);
+			            console.log(chalk.green("Bun in over?: ") + val[num].kitty.is_gestating);
 			            console.log(chalk.gray("__________________"));
+			            //console.log(val[num])
 			            console.log("");
 
 			        };
